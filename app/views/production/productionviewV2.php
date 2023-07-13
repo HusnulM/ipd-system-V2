60bacd3aa244f1eadd7e9b81098139c3190fc69b
HusnulM <husnulmub@gmail.com>
2023-07-13 22:20:19 +0700

    udpate

--- app/views/production/productionviewV2.php
+++ app/views/production/productionviewV2.php
@@ -16,7 +16,8 @@
                             </div>
                             <div class="body">
                                 <div class="row">                   
-                                    <div class="col-lg-6">
+                                    <!-- View Day 1 -->
+                                    <div class="col-lg-3">
                                         <table id="report-data" class="table table-bordered table-striped table-hover" style="width:100%;font-size:13px;">
                                             <thead>
                                                 <tr>
@@ -60,7 +61,7 @@
                                         </table>
                                     </div>
 
-                                    <div class="col-lg-6">
+                                    <div class="col-lg-3">
                                         <table id="report-data-night-shift" class="table table-bordered table-striped table-hover" style="width:100%;font-size:13px;">
                                             <thead>
                                                 <tr>
@@ -104,7 +105,97 @@
                                             </tbody>
                                         </table>
                                     </div>
+                                    
 
+                                    <!-- View Day 2 -->
+                                    <div class="col-lg-3">
+                                        <table id="report-data" class="table table-bordered table-striped table-hover" style="width:100%;font-size:13px;">
+                                            <thead>
+                                                <tr>
+                                                    <th colspan="3" style="text-align:center;">
+                                                        <?= $data['hdata']['date2']; ?>
+                                                    </th>
+                                                    <th colspan="3" style="text-align:center;">Day Shift</th>
+                                                </tr>
+                                                <tr>
+                                                    <th>Line</th>
+                                                    <th>Model</th>
+                                                    <th>Lot</th>
+                                                    <th>Plan Qty</th>
+                                                    <th>Output Qty</th>
+                                                    <th>Variance</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach ($data['rday2'] as $row) : ?>
+                                                    <?php if($row['planqtyd2s1'] != 0): ?>
+                                                    <tr>
+                                                        <td><?= $row['linename']; ?></td>
+                                                        <td><?= $row['model']; ?></td>
+                                                        <td><?= $row['lot_number']; ?></td>
+                                                        <td style="text-align:right;"><?= $row['planqtyd2s1']; ?></td>
+                                                        <?php if($row['qtyoutd2s1'] < $row['planqtyd2s1']): ?>
+                                                            <td style="background-color:red;color:white;text-align:right;"><?= $row['qtyoutd2s1']; ?></td>
+                                                        <?php elseif($row['qtyoutd2s1'] >= $row['planqtyd2s1'] && $row['qtyoutd2s1'] > 0): ?>
+                                                            <td style="background-color:green;color:white;text-align:right;">
+                                                                <?= $row['qtyoutd2s1']; ?>
+                                                            </td>
+                                                        <?php else: ?>
+                                                            <td style="text-align:right;"><?= $row['qtyoutd2s1']; ?></td>
+                                                        <?php endif; ?>
+
+                                                        <td style="text-align:right;"><?= $row['planqtyd2s1'] - $row['qtyoutd2s1']; ?></td>
+                                                    </tr>
+                                                    <?php endif; ?>
+                                                <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                    <div class="col-lg-3">
+                                        <table id="report-data-night-shift" class="table table-bordered table-striped table-hover" style="width:100%;font-size:13px;">
+                                            <thead>
+                                                <tr>
+                                                    <th colspan="3" style="text-align:center;">
+                                                        <?= $data['hdata']['date2']; ?>
+                                                    </th>
+                                                    <th colspan="3" style="text-align:center;">Night Shift</th>
+                                                </tr>
+                                                <tr>
+                                                    <th>Line</th>
+                                                    <th>Model</th>
+                                                    <th>Lot</th>
+                                                    <th>Plan Qty</th>
+                                                    <th>Output Qty</th>
+                                                    <th>Variance</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach ($data['rday2'] as $row) : ?>
+                                                    <?php if($row['planqtyd2s2'] != 0): ?>
+                                                    <tr>
+                                                        <td><?= $row['linename']; ?></td>
+                                                        <td><?= $row['model']; ?></td>
+                                                        <td><?= $row['lot_number']; ?></td>      
+                                                        <td style="text-align:right;"><?= $row['planqtyd2s2']; ?></td>                                                  
+                                                        <?php if($row['qtyoutd2s2'] < $row['planqtyd2s2']): ?>
+                                                            <td style="background-color:red;color:white;text-align:right;">
+                                                                <?= $row['qtyoutd2s2']; ?>
+                                                            </td>
+                                                        <?php elseif($row['qtyoutd2s2'] >= $row['planqtyd2s2'] && $row['qtyoutd2s2'] > 0): ?>
+                                                            <td style="background-color:green;color:white;text-align:right;">
+                                                                <?= $row['qtyoutd2s2']; ?>
+                                                            </td>
+                                                        <?php else: ?>
+                                                            <td style="text-align:right;"><?= $row['qtyoutd2s2']; ?></td>
+                                                        <?php endif; ?>
+                                                        <td style="text-align:right;"><?= $row['planqtyd2s2'] - $row['qtyoutd2s2']; ?></td>
+                                                    </tr>
+                                                    <?php endif; ?>
+                                                <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>  
                             </div>
                         </div>
